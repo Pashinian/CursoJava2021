fe7a2874e534acdf13f9a6249477ca67ae7a8c58
Pashinian <luisnavpash@gmail.com>
2021-09-16 18:14:59 +0200

    Mod2 ej3

--- Curso2021/src/modulo2/Ejercicio3.java
+++ Curso2021/src/modulo2/Ejercicio3.java
@@ -4,6 +4,12 @@ public class Ejercicio3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		char tipodiv;
+		byte goles;
+		int personas;
+		double promedio;
+		
 
 	}
 
