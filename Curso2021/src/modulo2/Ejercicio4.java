a0b1c61fce32aa2be7ddc29a92880e39305ab985
Pashinian <luisnavpash@gmail.com>
2021-09-16 18:45:06 +0200

    Mod2 ej4

---
+++ Curso2021/src/modulo2/Ejercicio4.java
@@ -0,0 +1,25 @@
+package modulo2;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte b = 10;
+		short s=20;
+		int i = 30;
+		long l= 40;
+		byte sumabb= (byte) (b + b);
+		short sumabs=(short) (b + s);
+		int sumabi= b + i; 
+		int sumaii= i + i; 
+		long sumasl= s + l;
+		
+		System.out.println(sumabb);
+		System.out.println(sumabs);
+		System.out.println(sumabi);
+		System.out.println(sumaii);
+		System.out.println(sumasl);
+	}
+
+}
---
+++ Curso2021/src/modulo2/Ejercicio5.java
@@ -0,0 +1,11 @@
+package modulo2;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
