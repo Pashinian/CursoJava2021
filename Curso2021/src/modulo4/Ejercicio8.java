6fd293099890329b9c28d79e26f509e31ab26dc1
luis <luis@luis-desktop>
2021-09-20 09:16:36 +0200

    8

---
+++ Curso2021/src/modulo4/Ejercicio8.java
@@ -0,0 +1,33 @@
+package modulo4;
+
+import java.util.Scanner;
+
+public class Ejercicio8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Jugador 1 elige Piedra(0), Papel(1), Tijera(2): ");
+		System.out.println("");
+		int jugador1 = input.nextInt(); 
+		System.out.print("Jugador 2 elige Piedra(0), Papel(1), Tijera(2): ");
+		System.out.println("");
+		int jugador2 = input.nextInt();
+		
+		if(jugador1 == 0 && jugador2 == 1) {
+			System.out.println("Gana el jugador 2");
+		}
+		else if(jugador1 == 1 && jugador2 == 2) {
+			System.out.println("Gana el jugador 2");
+		}
+		else if(jugador1 == 2 && jugador2 == 0) {
+			System.out.println("Gana el jugador 2");
+		}
+		else{
+			System.out.println("Gana el jugador 1");
+		}
+
+	}
+
+}
