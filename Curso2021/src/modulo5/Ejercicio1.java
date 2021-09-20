33f9b783e8a9f3cc2cd096e88746ac1e960d418f
luis <luis@luis-desktop>
2021-09-20 12:40:51 +0200

    Empieza modulo 5

---
+++ Curso2021/src/modulo5/Ejercicio1.java
@@ -0,0 +1,16 @@
+package modulo5;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hola = "Hola Mundo!";
+		
+		System.out.println(hola.toUpperCase());
+		System.out.println(hola.toLowerCase());
+		System.out.println(hola.replace('o', '2'));
+
+	}
+
+}
